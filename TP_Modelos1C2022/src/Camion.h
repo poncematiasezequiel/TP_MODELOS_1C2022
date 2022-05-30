f154903be5408565f39b63f79d33aa8c3cd8fb85
poncematiasezequiel <37522410+poncematiasezequiel@users.noreply.github.com>
2022-05-30 15:06:34 -0300

    Add files via upload

--- TP_Modelos1C2022/src/Camion.h
+++ TP_Modelos1C2022/src/Camion.h
@@ -1,7 +1,6 @@
 #include <list>
 #include "Sucursal.h"
 #include <vector>
-#include <fstream>
 
 using namespace std;
 
@@ -10,21 +9,21 @@ using namespace std;
 
 class Camion{
 public:
-	vector<Sucursal> circuitoFinal;
-	vector<Sucursal> sucursales;
+	list<Sucursal> circuitoFinal;
+	list<Sucursal> sucursales;
 	double monto = 0;
 	double capacidad = 0;
-	double dimension = 0;
 	Camion();
-	Camion(double capacidad, double dimension, vector<Sucursal> sucursales);
+	Camion(double capacidad, list<Sucursal> sucursales);
 	virtual ~Camion();
 	void sumarMonto(double montoS);
 	void recorrer();
 	void imprimir_circuito();
-	void recorrer_recursivo(Sucursal sucursal, list<Sucursal>* sucursalesSinVisitar);
-	bool destinoPosible(Sucursal* sucursal);
-	list<Sucursal>* destinosPosibles();
-	list<Sucursal> filtrarDestinos(list<Sucursal> sucursales);
+	void recorrer_recursivo(Sucursal sucursal, list<Sucursal> sucursalesSinVisitar);
+	bool destinoPosible(Sucursal sucursal);
+	list<Sucursal> destinosPosibles(Sucursal sucursal, list<Sucursal> sucursales);
+	void obtener_configuraciones(vector<vector<Sucursal>>& configuraciones, vector<Sucursal> sucursales);
 	void imprimir_lista(list<Sucursal> sucursales);
-	Sucursal obtenerSucursalMasProxima(Sucursal* sucursal, list<Sucursal>* sucursales);
+	Sucursal obtenerSucursalMasProxima(Sucursal sucursal, list<Sucursal> sucursales);
 };
+
